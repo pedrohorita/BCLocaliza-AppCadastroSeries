ab52dab28bafe046d3dfa0989e3e915e3cdf37b0
Pedro R T A Horita <pedro_horita@hotmail.com>
2021-03-17 16:17:38 -0300

    Criação projeto de cadastro de séries
    Criação do projeto console .NET para cadastro de séries.

---
+++ BCLocaliza-CadastroSeries/BCLocaliza-CadastroSeries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BCLocaliza_CadastroSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
